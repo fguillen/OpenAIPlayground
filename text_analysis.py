f8aa3b5e1dd037c57723126bd7d1762192015215
Fernando Guillen <fguillen.mail@gmail.com>
2024-06-02 20:00:28 +0200

    Text analysis working

--- text_analysis.py
+++ text_analysis.py
@@ -1,30 +1,28 @@
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
 import pandas as pd
 import os
 
 # Load environment variables from .env file
 load_dotenv()
 
-openai.api_key = os.getenv("API_KEY")
+client = OpenAI(api_key=os.getenv("API_KEY"))
 
 prompt = (
-    """Extract the following information from the text:
-    Nvidia's revenue
-    What Nvidia did this quarter
-    Remarks about AI"""
+  """Extract the following information from the text:
+  Nvidia's revenue
+  What Nvidia did this quarter
+  Remarks about AI"""
 )
 
 def extract_info(text):
-    completions = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "user", "content": prompt+"\n\n"+text}
-        ],
-        temperature=0.3,
-    )
-    message = completions.choices[0].message.content
-    return message
+  completions = client.chat.completions.create(
+    model="gpt-3.5-turbo-16k",
+    messages=[ {"role": "user", "content": prompt+"\n\n"+text} ],
+    temperature=0.3
+  )
+  message = completions#.choices[0].message.content
+  return message
 
 f = open("data/nvidia.txt", "r")
 txt_data = f.read()
